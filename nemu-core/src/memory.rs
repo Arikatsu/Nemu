829bc61ec3ee0a0c68276bbf8aa3fbcadd4ec969
Scald <104459145+Arikatsu@users.noreply.github.com>
2025-10-15 15:17:46 +0530

    refactor(memory): restructure memory layout and initialization

--- nemu-core/src/memory.rs
+++ nemu-core/src/memory.rs
@@ -1,61 +1,70 @@
 use crate::traits::Bus;
 
 pub struct Memory {
-    memory: [u8; 65536],
+    cartridge: [u8; 0x8000], // 32KB Cartridge ROM
+    vram: [u8; 0x2000],      // 8KB Video RAM
+    eram: [u8; 0x2000],      // 8KB External RAM
+    wram: [u8; 0x2000],      // 8KB Work RAM
+    oam: [u8; 0xA0],         // Sprite Attribute Table
+    io: [u8; 0x80],          // I/O Registers
+    hram: [u8; 0x7F],        // High RAM
+    ie: u8,                  // Interrupt Enable Register
 }
 
 impl Memory {
     pub fn new() -> Self {
-        let mut memory = [0; 65536];
-        // Test program starting at 0x0100
-        memory[0x0100] = 0x3E; // LD A, 0x42
-        memory[0x0101] = 0x42;
-
-        memory[0x0102] = 0x06; // LD B, 0x10
-        memory[0x0103] = 0x10;
-
-        memory[0x0104] = 0x01; // LD BC, 0xC050
-        memory[0x0105] = 0x50;
-        memory[0x0106] = 0xC0;
-
-        memory[0x0107] = 0x02; // LD (BC), A  -> writes 0x42 to 0xC050
-
-        memory[0x0108] = 0x03; // INC BC  -> BC becomes 0xC051
-
-        memory[0x0109] = 0x04; // INC B  -> B becomes 0xC1
-
-        memory[0x010A] = 0x05; // DEC B  -> B becomes 0xC0
-        memory[0x010B] = 0x05; // DEC B  -> B becomes 0xBF
-
-        memory[0x010C] = 0x3E; // LD A, 0x80
-        memory[0x010D] = 0x80;
-
-        memory[0x010E] = 0x07; // RLCA  -> A becomes 0x01, Carry set
-
-        memory[0x010F] = 0x3E; // LD A, 0x0F
-        memory[0x0110] = 0x0F;
-
-        memory[0x0111] = 0x06; // LD B, 0x01
-        memory[0x0112] = 0x01;
-
-        memory[0x0113] = 0x80; // ADD A, B  -> A = 0x10, Half-carry set
-
-        memory[0x0114] = 0x00; // NOP
-
-        Self { memory }
+        Self {
+            cartridge: [0; 0x8000],
+            vram: [0; 0x2000],
+            eram: [0; 0x2000],
+            wram: [0; 0x2000],
+            oam: [0; 0xA0],
+            io: [0; 0x80],
+            hram: [0; 0x7F],
+            ie: 0,
+        }
     }
 
     pub(crate) fn reset(&mut self) {
-        self.memory = [0; 65536];
+        self.cartridge = [0; 0x8000];
+        self.vram = [0; 0x2000];
+        self.eram = [0; 0x2000];
+        self.wram = [0; 0x2000];
+        self.oam = [0; 0xA0];
+        self.io = [0; 0x80];
+        self.hram = [0; 0x7F];
+        self.ie = 0;
     }
 }
 
 impl Bus for Memory {
     fn read(&self, addr: u16) -> u8 {
-        self.memory[addr as usize]
+        match addr {
+            0x0000..=0x7FFF => self.cartridge[addr as usize],
+            0x8000..=0x9FFF => self.vram[(addr - 0x8000) as usize],
+            0xA000..=0xBFFF => self.eram[(addr - 0xA000) as usize],
+            0xC000..=0xDFFF => self.wram[(addr - 0xC000) as usize],
+            0xE000..=0xFDFF => self.wram[(addr - 0xE000) as usize], // Echo RAM
+            0xFE00..=0xFE9F => self.oam[(addr - 0xFE00) as usize],
+            0xFEA0..=0xFEFF => 0, // unusable
+            0xFF00..=0xFF7F => self.io[(addr - 0xFF00) as usize],
+            0xFF80..=0xFFFE => self.hram[(addr - 0xFF80) as usize],
+            0xFFFF => self.ie,
+        }
     }
 
     fn write(&mut self, addr: u16, data: u8) {
-        self.memory[addr as usize] = data;
+        match addr {
+            0x0000..=0x7FFF => { /* ROM area (no write) */ }
+            0x8000..=0x9FFF => self.vram[(addr - 0x8000) as usize] = data,
+            0xA000..=0xBFFF => self.eram[(addr - 0xA000) as usize] = data,
+            0xC000..=0xDFFF => self.wram[(addr - 0xC000) as usize] = data,
+            0xE000..=0xFDFF => self.wram[(addr - 0xE000) as usize] = data, // Echo RAM
+            0xFE00..=0xFE9F => self.oam[(addr - 0xFE00) as usize] = data,
+            0xFEA0..=0xFEFF => { /* unusable */ }
+            0xFF00..=0xFF7F => self.io[(addr - 0xFF00) as usize] = data,
+            0xFF80..=0xFFFE => self.hram[(addr - 0xFF80) as usize] = data,
+            0xFFFF => self.ie = data,
+        }
     }
 }
