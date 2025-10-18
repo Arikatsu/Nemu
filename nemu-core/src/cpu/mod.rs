4ae6e68e7e26559fa5d9ca90baef10a6cbf0fc26
Scald <104459145+Arikatsu@users.noreply.github.com>
2025-10-18 22:56:24 +0530

    refactor(cpu): move InterruptMode and reg_cb macro to utils module

--- nemu-core/src/cpu/mod.rs
+++ nemu-core/src/cpu/mod.rs
@@ -1,36 +1,14 @@
 mod registers;
 mod instructions;
+mod utils;
 
 use instructions::*;
+use utils::*;
 use registers::{Reg8, Reg16, Registers};
 use crate::memory::Memory;
 
 #[cfg(feature = "debug_logging")]
 use std::io::{BufWriter, Write};
-
-pub(in self) enum InterruptMode {
-    Enabled,
-    Disabled,
-    Pending,
-}
-
-macro_rules! reg_cb {
-    ($code:expr) => {
-        {
-            match $code & 0x07 {
-                0 => Some(Reg8::B),
-                1 => Some(Reg8::C),
-                2 => Some(Reg8::D),
-                3 => Some(Reg8::E),
-                4 => Some(Reg8::H),
-                5 => Some(Reg8::L),
-                6 => None, // (HL)
-                7 => Some(Reg8::A),
-                _ => unreachable!(),
-            }
-        }
-    };
-}
 
 pub struct Cpu {
     pub(crate) regs: Registers,
