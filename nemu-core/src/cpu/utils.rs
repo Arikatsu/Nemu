fb77c26376f4e05f2a6d0cd2ed263338eb7d8e43
Scald <104459145+Arikatsu@users.noreply.github.com>
2026-01-01 18:15:51 +0530

    refactor(cpu): simplify opcode handling in execute_cb function

--- nemu-core/src/cpu/utils.rs
+++ nemu-core/src/cpu/utils.rs
@@ -4,23 +4,3 @@ pub(crate) enum InterruptMode {
     Disabled,
     Pending,
 }
-
-macro_rules! reg_cb {
-    ($code:expr) => {
-        {
-            match $code & 0x07 {
-                0 => Some(Reg8::B),
-                1 => Some(Reg8::C),
-                2 => Some(Reg8::D),
-                3 => Some(Reg8::E),
-                4 => Some(Reg8::H),
-                5 => Some(Reg8::L),
-                6 => None, // (HL)
-                7 => Some(Reg8::A),
-                _ => unreachable!(),
-            }
-        }
-    };
-}
-
-pub(in super) use reg_cb;
